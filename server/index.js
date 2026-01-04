5b6d5d0a6b8a45b249de254b9fc354e137a52e80
shellsri <shelly.sri18@gmail.com>
2026-01-04 17:54:42 +0530

    Make backend deployment ready

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./config/db.js";
 import bunkRoutes from "./routes/bunk.routes.js";
 import subjectRoutes from "./routes/subject.routes.js";
 import timetableRoutes from "./routes/timetable.routes.js";
 import deadlineRoutes from "./routes/deadline.routes.js";
 import healthRoutes from "./routes/health.routes.js";
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*", 
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/subjects", subjectRoutes);
@@ -19,6 +27,14 @@ app.use("/api/timetable", timetableRoutes);
 app.use("/api/deadlines", deadlineRoutes);
 app.use("/api/health", healthRoutes);
 app.use("/api/bunk", bunkRoutes);
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+
+app.get("/", (req, res) => {
+  res.send("Should I Bunk API is running 🚀");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
